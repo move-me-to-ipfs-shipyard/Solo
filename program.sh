07b2166d63138d270f75a612e14ce7dabb217ab9
Solo <Solo@galaxy-swindler>
2022-07-07 02:13:12 +0300

    i remove commit count from release - my .jar is entirely defined by commit hash - we are ageless and evergreen in the garden :Henning-Wehn well, i kept the box, didn't i?

--- program.sh
+++ program.sh
@@ -14,24 +14,13 @@ main(){
     -M -m Solo.main
 }
 
-tag(){
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
-  TAG="$COMMIT_COUNT-$COMMIT_HASH"
-  git tag $TAG $COMMIT_HASH
-  echo $COMMIT_HASH
-  echo $TAG
-}
-
 jar(){
 
   rm -rf out/*.jar out/classes
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
   clojure \
     -X:Genie Genie.core/process \
     :main-ns Solo.main \
-    :filename "\"out/Solo-$COMMIT_COUNT-$COMMIT_HASH.jar\"" \
+    :filename "\"out/Solo-$(git rev-parse --short HEAD).jar\"" \
     :paths '["src"]'
 }
 
